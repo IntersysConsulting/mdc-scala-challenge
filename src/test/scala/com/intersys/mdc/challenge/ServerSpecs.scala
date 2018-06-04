069893357cdb8b1d7d12e5e131e37f668beefde1
rhdzmota <rohdzmota@gmail.com>
2018-06-04 12:10:37 -0500

    Hello World Test
    Usage of the akka testkit to test the Hello World service. Run `sbt
    test`.

--- src/test/scala/com/intersys/mdc/challenge/ServerSpecs.scala
+++ src/test/scala/com/intersys/mdc/challenge/ServerSpecs.scala
@@ -5,7 +5,7 @@ import  org.scalatest.{Matchers, WordSpec}
 
 class ServerSpecs extends WordSpec with Matchers with ScalatestRouteTest {
   "The Service" should {
-    "Return the hello world string" in {
+    "return the hello world string" in {
       Get("/hello") ~> Server.route ~> check {
         val response   = responseAs[String]
         val helloWorld = "<h1>Hello World!</h1>"
