287c123097c18181e37fae314c35ae11223c60e4
rhdzmota <rohdzmota@gmail.com>
2018-06-05 19:15:15 -0500

    Json Support Trait
    Add a JsonSupport Trait with implicit values for json formatting. Any
    problem definition should extend from this trait.

---
+++ src/main/scala/com/intersys/mdc/challenge/exercises/problems/JsonSupport.scala
@@ -0,0 +1,17 @@
+package com.intersys.mdc.challenge.exercises.problems
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import com.intersys.mdc.challenge.exercises.problems.Example.FibonacciResult
+import spray.json.DefaultJsonProtocol
+//import com.intersys.mdc.challenge.exercises.problems.Problem1.MixedString
+//import com.intersys.mdc.challenge.exercises.problems.Problem2.SuperDigit
+//import com.intersys.mdc.challenge.exercises.problems.Problem4.Calculation
+//import com.intersys.mdc.challenge.exercises.problems.Problem5.IntListResult
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val jsonFormatFibonacciResults = jsonFormat1(FibonacciResult)
+  //implicit val jsonFormatMixedString = jsonFormat3(MixedString)
+  //implicit val jsonFormatSuperDigit = jsonFormat3(SuperDigit)
+  //implicit val jsonFormatCalculation = jsonFormat4(Calculation)
+  //implicit val jsonFormatIntListResult = jsonFormat4(IntListResult)
+}
