e65b18d181a1abc5ffe619e8531520264141c53e
rhdzmota <rohdzmota@gmail.com>
2018-06-05 20:43:13 -0500

    Problem 5
    * Create the definition and solutions of the problem 5.
    * Add the IntListResult implicit json formatter to the JsonSupport
    trait.
    * Add the problem 5 solution route to the Exercises router.

--- src/main/scala/com/intersys/mdc/challenge/exercises/problems/JsonSupport.scala
+++ src/main/scala/com/intersys/mdc/challenge/exercises/problems/JsonSupport.scala
@@ -6,12 +6,12 @@ import spray.json.DefaultJsonProtocol
 import com.intersys.mdc.challenge.exercises.problems.Problem1.MixedString
 import com.intersys.mdc.challenge.exercises.problems.Problem2.SuperDigit
 import com.intersys.mdc.challenge.exercises.problems.Problem4.Calculation
-//import com.intersys.mdc.challenge.exercises.problems.Problem5.IntListResult
+import com.intersys.mdc.challenge.exercises.problems.Problem5.IntListResult
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val jsonFormatFibonacciResults = jsonFormat1(FibonacciResult)
   implicit val jsonFormatMixedString = jsonFormat3(MixedString)
   implicit val jsonFormatSuperDigit = jsonFormat3(SuperDigit)
   implicit val jsonFormatCalculation = jsonFormat4(Calculation)
-  //implicit val jsonFormatIntListResult = jsonFormat4(IntListResult)
+  implicit val jsonFormatIntListResult = jsonFormat4(IntListResult)
 }
