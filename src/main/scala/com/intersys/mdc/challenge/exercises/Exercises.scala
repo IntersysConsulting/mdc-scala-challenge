26e8c57d78a6f44c9ea2f1a64c64f93e41d2f95d
rhdzmota <rohdzmota@gmail.com>
2018-06-05 19:54:42 -0500

    Problem 2
    * Create problem 2 with solution.
    * Create an implicit implementation for SuperDigit json formatting in
    JsonSupport trait.
    * Add the problem 2 solution route to the Exercices controller.

--- src/main/scala/com/intersys/mdc/challenge/exercises/Exercises.scala
+++ src/main/scala/com/intersys/mdc/challenge/exercises/Exercises.scala
@@ -7,8 +7,8 @@ import akka.http.scaladsl.server.Directives._
 case object Exercises {
   val route: Route = pathPrefix("problems") {
     Example.solution ~
-    Problem1.solution //~
-    //Problem2.solution ~
+    Problem1.solution ~
+    Problem2.solution //~
     //Problem3.solution ~
     //Problem4.solution ~
     //Problem5.solution
