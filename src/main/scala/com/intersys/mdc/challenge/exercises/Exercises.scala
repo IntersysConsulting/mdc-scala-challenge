a4c478c91be6759ac4916e95447275ed27d40c3d
rhdzmota <rohdzmota@gmail.com>
2018-06-05 19:20:29 -0500

    Exercises Controller
    The Exercices object serves as a controller for all the problem's
    solution route.

---
+++ src/main/scala/com/intersys/mdc/challenge/exercises/Exercises.scala
@@ -0,0 +1,16 @@
+package com.intersys.mdc.challenge.exercises
+
+import com.intersys.mdc.challenge.exercises.problems._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+
+case object Exercises {
+  val route: Route = pathPrefix("problems") {
+    Example.solution  //~
+    //Problem1.solution ~
+    //Problem2.solution ~
+    //Problem3.solution ~
+    //Problem4.solution ~
+    //Problem5.solution
+  }
+}
